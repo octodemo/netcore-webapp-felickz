71b81647d0cf133d4c943ecf1ffdc24034119a20
Chad Bentz <1760475+felickz@users.noreply.github.com>
2024-09-26 22:31:32 -0400

    more logging scenarios

--- LogForgingMiddleware.cs
+++ LogForgingMiddleware.cs
@@ -8,19 +8,45 @@ public class LogForgingMiddleware
     private readonly RequestDelegate _next;
     private readonly ILogger<LogForgingMiddleware> _logger;
 
+    public enum RbacRole
+    {
+        Admin,
+        User,
+        Guest
+    }
+
+
     public LogForgingMiddleware(RequestDelegate next, ILogger<LogForgingMiddleware> logger)
     {
         _next = next;
         _logger = logger;
     }
 
+
+
     public async Task InvokeAsync(HttpContext context)
     {
-        string username = context.Request.Query["username"];
+        string role = context.Request.Query["role"];
+
         // BAD: User input logged as-is
-        _logger.LogWarning(username + " log in requested.");
+        _logger.LogWarning(role + " log in requested.");
+
         // GOOD: User input logged with new-lines removed
-        _logger.LogWarning(username?.Replace(Environment.NewLine, "") + " log in requested");
+        _logger.LogWarning(role?.Replace(Environment.NewLine, "") + " log in requested");
+
+        // GOOD: cast to enum
+        _logger.LogWarning((RbacRole)Enum.Parse(typeof(RbacRole), role) + " log in requested");
+
+        // GOOD: cast to enum / tryparse
+        if (Enum.TryParse(typeof(RbacRole), role, out var parsedRole))
+        {
+            _logger.LogWarning("{Role} log in requested.", (RbacRole)parsedRole);
+        }
+        else
+        {
+            // GOOD: User input logged with new-lines removed
+            _logger.LogWarning(role?.Replace(Environment.NewLine, "") + " log in requested");
+        }
 
         await _next(context);
     }
